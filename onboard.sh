d34e989996bdf3d83d4f905043b8b87484c11c27
SIG Demo User <74533117+siguser8@users.noreply.github.com>
2021-03-31 11:42:44 -0500

    Update onboard.sh

--- onboard.sh
+++ onboard.sh
@@ -6,9 +6,3 @@
 	--username=siguser8 \
 	--password=SIGpass8! \
 	--io.asset.id=github-io-sample
-
-# Save the following values for further use:
-# assetId: github-io-sample
-# IO_ACCESS_TOKEN: 77768198-e0c7-43d8-a03a-625c7ed82404
-# WORKFLOW_ENGINE_ACCESS_TOKEN: e9a05a46-b2d3-46ac-9b43-7d0e4cd142a0
-# INFO: ApplicationManifest.yml is generated. Please update the source code management details in it and add the file to the root of the project.
