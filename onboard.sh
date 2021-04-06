acd60174ad6bc0bb2c1d1ecb30df427a2fa321de
SIG Demo User <74533117+siguser8@users.noreply.github.com>
2021-04-06 12:23:48 -0500

    Delete onboard.sh

--- onboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-../io-prescription/io-scripts/onboarding.sh \
-	--io.url=http://f13f64e8a30d.ngrok.io \
-	--workflow.engine.url=http://8a3ff2d2893a.ngrok.io \
-	--username=siguser8 \
-	--password=SIGpass8! \
-	--io.asset.id=github-io-sample
